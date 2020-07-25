d27cb6ba1c6448610b1f90eb32b9c7dea6979485
kramskovdaniil <kramskov.daniil@yandex.ru>
2020-07-25 21:11:30 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,6 @@
+from urllib.request import urlopen, Request
+from settings import *
+
+
+if __name__ == '__main__':
+    pass
---
+++ settings.py
@@ -0,0 +1,23 @@
+URL = "https://vk.com/@yvkurse"
+headers = {
+        "Host": "vk.com",
+        "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+        "Accept-Language": "ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Authorization": "Basic XzoxMzUwYzFiZjE1ZWZmZmFhZmMzZDQxM2ZhYmIwNTQ1MTNkYzg1OGJjYz"
+                         "MxYmJhYThmMzJhN2VlNGQ5YmZkZmM2YjM5YTM=",
+        "Connection": "keep-alive",
+        "Cookie": "remixlang=0; remixstid=113922131_Oed97yACTpIsuYWDfsjz2FxrWOdycSJ97xwUUb7DNvH;"
+                  " remixflash=32.0.0; remixscreen_width=1472; remixscreen_height=828;"
+                  " remixscreen_dpr=1.3043478260869565; remixscreen_depth=24; remixscreen_orient=1;"
+                  " remixscreen_winzoom=1; remixdt=7200; tmr_reqNum=3928; tmr_lvid=056591b2332d91e003f63cb26889fb8d; "
+                  "tmr_lvidTS=1591125781311; remixusid=NTAwODRmOTY2NjdkYWVhODRhYjFkYjk2; remixseenads=0; "
+                  "remixua=154%7C-1%7C290%7C1062540685; remixrefkey=1f91621b5401391f78;"
+                  " remixmdevice=1920/1080/1/!!-!!!!; _ga=GA1.2.696056315.1592473808; _fbp=fb.1.1592473808783.80821706;"
+                  " remixff=101; remixab=1; tmr_detect=1%7C1595691298826; remixgp=de7fccd8b2776f3c00be3b6769151470;"
+                  " remixtu=40cce5dfe4c99e9c77; remixaudio_show_alert_today=0; remixbdr=1; remixlhk=b495673f97bb5d039d",
+        "Upgrade-Insecure-Requests": 1,
+        "Cache-Control": "max-age=0",
+        "TE": "Trailers",
+}
